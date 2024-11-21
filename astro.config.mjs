d7d3630988590309deead9e059ba9452e689193b
Kim Branzell <hej@jagheter.kim>
2024-11-21 21:12:00 +0100

    Lägger till base

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kimbranzell.github.io/KaffeKompis/',
-  output: 'static',
+  site: 'https://kimbranzell.github.io',
+  base: '/KaffeKompis/',
   build: {
     assets: 'assets'
   },
